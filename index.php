818bb845c41110269b1bf2cdf8a5595919ff706b
zharnikov <zharnikov@mbgbeauty.ru>
2018-09-18 18:33:07 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<h1>12345</h1>
+<select>
+	<?php
+	for($i = 1910; $i < 2010; $i++){
+		echo "<option>$i</option>";
+	}
+	?>
+</select>
+<?php
+	echo 'Hello, World!';
+	
+?>
+<hr>
+<p>&copy; 2010 - <?php echo date('Y'); ?></p>
