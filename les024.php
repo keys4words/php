856890397371494e6d7df20eb9f80868559042dc
Max 19038 <keys4words@gmail.com>
2018-10-27 23:41:41 +0300

    add simple form

--- les024.php
+++ les024.php
@@ -1,22 +1,13 @@
 <?php
+	require_once 'lib/functions.php';
 	$summa = false;
 	$x = false;
 	$y = false;
 	if(isset($_POST["myform"])){
 		$x = $_POST["x"] ?? false;
 		$y = $_POST["y"]?? false;
-		if($x !== false && $y !== false && is_numeric($x) && is_numeric($y)) $summa = $x + $y;
+		$summa = summa($x, $y);
 	}
 ?>
 <?php if ($summa !== false) : ?><p>Sum is <?=$summa?></p><?php endif ?>
-<form name="myform" action="<?=$_SERVER["REQUEST_URI"]?>" method="post">
-	<div>
-		Input first number:<input type="text" name="x" value="<?=$x?>">
-	</div>
-	<div>
-		Input second number:<input type="text" name="y" value="<?=$y?>">
-	</div>
-	<div>
-		<input type="submit" name="myform" value="Calc">
-	</div>
-</form>
+<?php include 'form.php' ?>
