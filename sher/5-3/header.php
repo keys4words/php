8228cd4f247be17be241badadd18a711118eaf0a
zharnikov <zharnikov@mbgbeauty.ru>
2019-03-13 19:05:01 +0300

    start les 5-3

---
+++ sher/5-3/header.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'main.php';
+require_once 'start_mysql.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>print $title</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
