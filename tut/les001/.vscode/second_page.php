a13bb906a7e96a5d02e1d549a3118d30135afaa5
zharnikov <zharnikov@mbgbeauty.ru>
2019-03-22 19:49:28 +0300

    add transfer to second page

---
+++ tut/les001/.vscode/second_page.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>second page</title>
+</head>
+<body>
+    
+    <pre>
+    <?php
+        //print_r($_GET);
+        $id = $_GET['id'];
+        echo $id;
+        ?>
+    </pre>
+
+    
+</body>
+</html>
