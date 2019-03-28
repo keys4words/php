09c3a115c89e2e75ebf710b213bec9d9a1717008
zharnikov <zharnikov@mbgbeauty.ru>
2019-03-28 19:13:42 +0300

    add validation to form

--- tut/les001/.vscode/validations.php
+++ tut/les001/.vscode/validations.php
@@ -8,41 +8,46 @@
 </head>
 <body>
     <?php
-        $value = '';
-        if(!isset($value) || empty($value))
+        $value = trim("  0");
+        if(!isset($value) || empty($value) && !is_numeric($value))
         {
             echo "Validation failed.<br>";
         }
-        $value = "";
-        $min = 3;
-        if(strlen($value) < $min){
-            echo "Validation failed.<br>";
-        }
-        $max = 6;
-        if(strlen($value) > $max){
-            echo "Validation failed.<br>";
-        }
-        $value = "";
-        if(!is_string($value)){
-            echo "Validation failed.<br>";
-        }
 
-        $value = "1";
-        $set = array("1", "2", "3", "4");
-        if(!in_array($value, $set)){
+        if(!isset($value) || $value === "")
+        {
             echo "Validation failed.<br>";
         }
+        // $value = "";
+        // $min = 3;
+        // if(strlen($value) < $min){
+        //     echo "Validation failed.<br>";
+        // }
+        // $max = 6;
+        // if(strlen($value) > $max){
+        //     echo "Validation failed.<br>";
+        // }
+        // $value = "";
+        // if(!is_string($value)){
+        //     echo "Validation failed.<br>";
+        // }
 
-        if(preg_match("/PHP/", "PHP is fun")){
-            echo "A match was found.<br>";
-        } else {
-            echo "A match was NOT found.<br>";
-        }
+        // $value = "1";
+        // $set = array("1", "2", "3", "4");
+        // if(!in_array($value, $set)){
+        //     echo "Validation failed.<br>";
+        // }
 
-        $value = "nobody@nowhere.com";
-        if(!preg_match("/@/", $value)){
-            echo "Validation failed.<br>";
-        }
+        // if(preg_match("/PHP/", "PHP is fun")){
+        //     echo "A match was found.<br>";
+        // } else {
+        //     echo "A match was NOT found.<br>";
+        // }
+
+        // $value = "nobody@nowhere.com";
+        // if(!preg_match("/@/", $value)){
+        //     echo "Validation failed.<br>";
+        // }
     ?>
 </body>
 </html>
